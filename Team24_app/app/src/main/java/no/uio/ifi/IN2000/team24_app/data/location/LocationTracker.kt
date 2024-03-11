9d0bcbbd7674987898bb6add0e7044dc0ea16348
halvornj <halvornjaatad@gmail.com>
2024-03-11 20:06:17 +0100

    check for coarse location or gps_enabled

--- Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/data/location/LocationTracker.kt
+++ Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/data/location/LocationTracker.kt
@@ -12,18 +12,29 @@ class LocationTracker(
     private val fusedLocationClient: FusedLocationProviderClient,
     private val application: Application
 ){
+    //if this returns null, remember to create a default
     suspend fun getLocation(): Location?{
 
+        //first, check if we have permission to access the coarse location. no need to try fine location, the forecast isn't that granular anyway
         val hasAccessCoarseLocationPermission = ContextCompat.checkSelfPermission(
             application,
             android.Manifest.permission.ACCESS_COARSE_LOCATION
         ) == PackageManager.PERMISSION_GRANTED
 
+
         val locationManager = application.getSystemService(
             Context.LOCATION_SERVICE
         ) as LocationManager
 
+        //check if we have some way of accessing gps: cellular or network
+        val isGpsEnabled = locationManager
+            .isProviderEnabled(LocationManager.NETWORK_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
 
+        //if we can't get gps or coarse location, give up (handle error on received null in middleman-class
+        if(!isGpsEnabled && !hasAccessCoarseLocationPermission){
+            return null
+        }
         //TODO actually get location
     }
 }
