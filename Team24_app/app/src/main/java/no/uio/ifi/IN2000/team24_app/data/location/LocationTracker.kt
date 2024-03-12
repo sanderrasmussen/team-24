cd40e79899501ee3b62a9b340475006753e5daa0
halvornj <halvornjaatad@gmail.com>
2024-03-12 19:07:57 +0100

    viewmodel calls the locationtracker-code to get position

--- Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/data/location/LocationTracker.kt
+++ Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/data/location/LocationTracker.kt
@@ -1,30 +1,31 @@
 package no.uio.ifi.IN2000.team24_app.data.location
 
-import android.app.Application
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 
 class LocationTracker(
-    private val fusedLocationClient: FusedLocationProviderClient,
-    private val application: Application
+    private val context: Context
 ){
+    private val fusedLocationClient: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+
     //if this returns null, remember to create a default
     suspend fun getLocation(): Location?{
 
         //first, check if we have permission to access the coarse location. no need to try fine location, the forecast isn't that granular anyway
         val hasAccessCoarseLocationPermission = ContextCompat.checkSelfPermission(
-            application,
+            context,
             android.Manifest.permission.ACCESS_COARSE_LOCATION
         ) == PackageManager.PERMISSION_GRANTED
 
 
-        val locationManager = application.getSystemService(
+        val locationManager = context.getSystemService(
             Context.LOCATION_SERVICE
         ) as LocationManager
 
--- Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/ui/home/HomeScreenViewModel.kt
+++ Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/ui/home/HomeScreenViewModel.kt
@@ -1,15 +1,35 @@
 package no.uio.ifi.IN2000.team24_app.ui.home
 
+import android.content.Context
 import android.location.Location
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import no.uio.ifi.IN2000.team24_app.data.location.LocationTracker
+import no.uio.ifi.IN2000.team24_app.data.locationForecast.LocationForecast
 
+class HomeScreenViewModel(
+    private val fusedLocationClient: FusedLocationProviderClient,
 
-class HomeScreenViewModel(private val fusedLocationClient: FusedLocationProviderClient){
+    //this state and the calling function are more just to check that the LocationTracker functions, cant test in junit as it needs the actual application
+    private val _weatherState: MutableStateFlow<LocationForecast>,
+    var weatherState:StateFlow<LocationForecast> = _weatherState.asStateFlow()
 
-    private fun getPosition(){
-        fusedLocationClient.lastLocation.addOnSuccessListener {location->
-            //can in rare cases be null
-        }
+): ViewModel(){
+
+     fun getPosition(context:Context){
+         viewModelScope.launch(Dispatchers.IO){
+            val pos = LocationTracker( context)
+         }
     }
 }
