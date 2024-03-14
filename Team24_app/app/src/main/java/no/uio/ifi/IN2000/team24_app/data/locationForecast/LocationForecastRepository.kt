7cf63fdc823cc23890e5ba57c9ede03ed014b75a
sanderrasmussen <sander.rasmussn@gmail.com>
2024-03-14 12:19:41 +0100

    test file now works without errors, still have not viewed the content though

--- Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/data/locationForecast/LocationForecastRepository.kt
+++ Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/data/locationForecast/LocationForecastRepository.kt
@@ -1,17 +1,23 @@
 package no.uio.ifi.IN2000.team24_app.data.locationForecast
 
+import kotlinx.serialization.SerialName
 
 
-data class weatherNow(
-    var time : String,
-
+data class WeatherNow(
+    var time : String? = null,
+    var air_pressure_at_sea_level : Double? = null,
+    var air_temperature : Double? = null,
+    var cloud_area_fraction : Double? = null,
+    var relative_humidity : Double? = null,
+    var wind_from_direction : Double? = null,
+    var wind_speed : Double? = null
 )
 class LocationForecastRepository{
     val dataSource : LocationForecastDatasource = LocationForecastDatasource()
     //still unsure how often this hould be updated
     var locationForecast : LocationForecast? = null
     var ForecastMap : HashMap<String?, ArrayList<Timeseries>>? = HashMap<String?, ArrayList<Timeseries>>() //dato som nøkkel og timeseries som verdi
-    
+    var weatherNow : WeatherNow? = null
     //re-fetching api every hour is what i have in mind
     suspend fun UpdateLocationForecastObject(lat:Double, lon: Double) {
         //get forecast object
@@ -26,6 +32,7 @@ class LocationForecastRepository{
         return getProperties()?.timeseries
     }
 
+   
     fun organizeForecastIntoMapByDay(){
         getTimeseries()?.forEach{
             var date = it.time?.split("T")?.get(0)
