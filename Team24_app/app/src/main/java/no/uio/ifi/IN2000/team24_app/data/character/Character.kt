ec9468d28125c138e602145b659d2f0f6250cc50
halvornj <halvornjaatad@gmail.com>
2024-03-18 10:53:03 +0100

    composable character-function

--- Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/data/character/Character.kt
+++ Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/data/character/Character.kt
@@ -1,3 +1,14 @@
 package no.uio.ifi.IN2000.team24_app.data.character
 
-data class Character(val head: Head, val torso: Torso, val legs: Legs)
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+
+data class Character(val head: Head, val torso: Torso, val legs: Legs) {
+
+    @Composable
+    fun Player() {
+        LazyColumn {
+        }
+    }
+}
