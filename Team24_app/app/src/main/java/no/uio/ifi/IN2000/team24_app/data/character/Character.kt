f6f817c0fb6b7034ccdf02209f9cc3661cbfc595
halvornj <halvornjaatad@gmail.com>
2024-03-18 11:23:44 +0100

    trying to get drawable weatherIcons to work??

--- Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/data/character/Character.kt
+++ Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/data/character/Character.kt
@@ -3,12 +3,20 @@ package no.uio.ifi.IN2000.team24_app.data.character
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import coil.compose.SubcomposeAsyncImage
+import no.uio.ifi.IN2000.team24_app.R
 
 data class Character(val head: Head, val torso: Torso, val legs: Legs) {
 
     @Composable
     fun Player() {
         LazyColumn {
+
+            SubcomposeAsyncImage(
+                model = R.drawable.
+            )
+
+
         }
     }
 }
