962d81e02ea3746f68d279271f15b570f064949a
Hifza Mehmood <hifzame@uio.no>
2024-04-17 23:59:15 +0200

    functioning components + made darkmode and lightmode public in theme

--- Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/ui/settings/SettingsScreen.kt
+++ Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/ui/settings/SettingsScreen.kt
@@ -1,108 +1,152 @@
 package no.uio.ifi.IN2000.team24_app.ui.settings
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ColorScheme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+
+
+object Translations {
+    val texts: Map<String, String> = mapOf(
+        "screen_name" to "Settings",
+        "dark_mode" to "Dark Mode",
+        "language" to "Language",
+        "temperature" to "Temperature",
+        "text_size" to "Text Size",
+
+    )
+}
+object TranslationsNo {
+    val texts: Map<String, String> = mapOf(
+        "screen_name" to "Innstillinger",
+        "dark_mode" to "Mørk modus",
+        "language" to "Språk",
+        "temperature" to "Temperatur",
+        "text_size" to "Tekststørrelse",
+
+    )
+}
+
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SettingsScreen(){
+fun SettingsScreen() {
+    var selectedTextSize by remember { mutableStateOf(18.sp) }
+    var isNorwegian by remember{ mutableStateOf(false)}
+    val texts = if (isNorwegian) TranslationsNo.texts else Translations.texts
+    var isDarkMode by remember { mutableStateOf(false) }
+
+    val colorScheme = if (isDarkMode) DarkColorScheme else LightColorScheme
 
     Scaffold(
         modifier = Modifier,
         topBar = {
             CenterAlignedTopAppBar(
                 colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
+                    containerColor = colorScheme.primaryContainer,
+                    titleContentColor = colorScheme.primary
                 ),
                 title = {
-                    Text(
-                        "Settings"
-                    )
-                },
-
-                )
+                    texts["screen_name"]?.let {
+                        Text(
+                            text = it,
+                            color = colorScheme.primary
+                        )
+                    }
+                }
+            )
         },
     ) { innerPadding ->
-        LazyColumn(
-            modifier = Modifier.padding(innerPadding),
-            verticalArrangement = Arrangement.spacedBy(25.dp),
-
-            content = {
-                item {
-
-                    DarkMode()
-
-                    Language()
-
-                    CelorFah()
-
-                    TextSize()
-
-
-                }
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .padding(innerPadding)
+                .background(color = colorScheme.background),
+            verticalArrangement = Arrangement.SpaceEvenly,)
+            {
+                    DarkMode(colorScheme, texts, selectedTextSize, isDarkMode) {
+                        isDarkMode = it
+                    }
+                    Language(colorScheme,texts, selectedTextSize) { isNorwegian = it }
+                    CelorFah(colorScheme,texts, selectedTextSize)
+                    TextSize(colorScheme,texts, selectedTextSize) { newSize ->
+                        selectedTextSize = newSize
+                    }
 
             }
+    }
+}
+
+
+
+/*
+    @Composable
+    fun SwitchComponent(
+        isChecked: Boolean,
+        onCheckedChange: (Boolean) -> Unit
+    ) {
+        Switch(
+            checked = isChecked,
+            onCheckedChange = onCheckedChange
+        )
+    }
+*/
+@Composable
+fun DarkMode(
+    color: ColorScheme,
+    texts: Map<String, String>,
+    selectedTextSize: TextUnit,
+    isDarkMode: Boolean,
+    onDarkModeToggle: (Boolean) -> Unit
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.Bottom
+    ) {
+        texts["dark_mode"]?.let {
+            Text(
+                text = it,
+                color = color.primary,
+                fontSize = selectedTextSize
+            )
+        }
+        Switch(
+            checked = isDarkMode,
+            onCheckedChange = onDarkModeToggle
         )
     }
 }
 
 @Composable
-fun SwitchComponent() {
-    var isChecked by remember { mutableStateOf(false) }
-    Switch(
-        checked = isChecked,
-        onCheckedChange = { isChecked = it },
-    )
-}
-
-@Composable
-fun DarkMode(){
-    Spacer(modifier = Modifier.width(16.dp))
-    Row(
-        modifier =  Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.Bottom
-    ){
-        Text(text= "Dark Mode",
-            color= Color.Black,
-            fontSize= 25.sp)
-
-        SwitchComponent()
-    }
-
-}
-
-@Composable
-fun Language(){
-    var norwegian by remember { mutableStateOf(true) }
-    var english by remember { mutableStateOf(false) }
+fun Language(color: ColorScheme,texts : Map <String, String>, selectedTextSize: TextUnit, onLanguageSelected: (Boolean) -> Unit) {
+    var norwegian by remember { mutableStateOf(false) }
+    var english by remember { mutableStateOf(true) }
     Row(
         modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceEvenly,
@@ -110,29 +154,34 @@ fun Language(){
 
 
     ) {
-        Text(text= "Language",
-            color= Color.Black,
-            fontSize= 25.sp)
-
-        Text(
-            text = "No",
-            color = if (norwegian) Color.Black else Color.Gray,
-            fontSize = if (norwegian) 20.sp else 18.sp,
-            fontWeight = if (norwegian) FontWeight.Bold else FontWeight.Normal,
-            modifier = Modifier.clickable {
-                norwegian = true
-                english = false
-            }
-        )
+        texts["language"]?.let {
+            Text(
+                text = it,
+                color = color.primary,
+                fontSize = selectedTextSize
+            )
+        }
 
         Text(
             text = "En",
-            color = if (english) Color.Black else Color.Gray,
-            fontSize = if (english) 20.sp else 18.sp,
+            color = if (english) color.primary else Color.Gray,
+            fontSize = selectedTextSize,
             fontWeight = if (english) FontWeight.Bold else FontWeight.Normal,
             modifier = Modifier.clickable {
+                onLanguageSelected(false)
                 norwegian = false
                 english = true
+            }
+        )
+        Text(
+            text = "No",
+            color = if (norwegian) color.primary else Color.Gray,
+            fontSize = selectedTextSize,
+            fontWeight = if (norwegian) FontWeight.Bold else FontWeight.Normal,
+            modifier = Modifier.clickable {
+                onLanguageSelected(true)
+                norwegian = true
+                english = false
             }
         )
     }
@@ -140,23 +189,27 @@ fun Language(){
 
 
 @Composable
-fun CelorFah(){
+fun CelorFah(color: ColorScheme,texts : Map <String, String>, selectedTextSize: TextUnit) {
     var celsius by remember { mutableStateOf(true) }
     var fahrenheit by remember { mutableStateOf(false) }
     Row(
-        modifier =  Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceEvenly,
         verticalAlignment = Alignment.Bottom
 
     ) {
-        Text(text= "Temperature",
-            color= Color.Black,
-            fontSize= 25.sp)
+        texts["temperature"]?.let {
+            Text(
+                text = it,
+                color = color.primary,
+                fontSize = selectedTextSize
+            )
+        }
 
         Text(
             text = "C°",
-            color = if (celsius) Color.Black else Color.Gray,
-            fontSize = if(celsius) 20.sp else 18.sp,
+            color = if (celsius) color.primary else Color.Gray,
+            fontSize = selectedTextSize,
             fontWeight = if (celsius) FontWeight.Bold else FontWeight.Normal,
             modifier = Modifier.clickable {
                 celsius = true
@@ -166,8 +219,8 @@ fun CelorFah(){
 
         Text(
             text = "F°",
-            color = if (fahrenheit) Color.Black else Color.Gray,
-            fontSize = if(fahrenheit) 20.sp else 18.sp,
+            color = if (fahrenheit) color.primary else Color.Gray,
+            fontSize = selectedTextSize,
             fontWeight = if (fahrenheit) FontWeight.Bold else FontWeight.Normal,
             modifier = Modifier.clickable {
                 celsius = false
@@ -179,26 +232,32 @@ fun CelorFah(){
 }
 
 @Composable
-fun TextSize(){
-    var small by remember { mutableStateOf(true) }
+fun TextSize(color: ColorScheme,texts : Map <String, String>,selectedTextSize: TextUnit, onTextSizeSelected: (TextUnit) -> Unit) {
+    var small by remember { mutableStateOf(false) }
     var medium by remember { mutableStateOf(false) }
     var large by remember { mutableStateOf(false) }
+
     Row(
-        modifier =  Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceEvenly,
         verticalAlignment = Alignment.Bottom
 
     ) {
-        Text(text= "Text Size",
-            color= Color.Black,
-            fontSize= 25.sp)
+        texts["text_size"]?.let {
+            Text(
+                text = it,
+                color = color.primary,
+                fontSize = selectedTextSize
+            )
+        }
 
         Text(
             text = "Abc",
-            color = if (small) Color.Black else Color.Gray,
-            fontSize = 16.sp ,
+            color = if (small) color.primary else Color.Gray,
+            fontSize = 16.sp,
             fontWeight = if (small) FontWeight.Bold else FontWeight.Normal,
             modifier = Modifier.clickable {
+                onTextSizeSelected(16.sp)
                 small = true
                 medium = false
                 large = false
@@ -207,10 +266,11 @@ fun TextSize(){
 
         Text(
             text = "Abc",
-            color = if (medium) Color.Black else Color.Gray,
+            color = if (medium) color.primary else Color.Gray,
             fontSize = 18.sp,
             fontWeight = if (medium) FontWeight.Bold else FontWeight.Normal,
             modifier = Modifier.clickable {
+                onTextSizeSelected(18.sp)
                 small = false
                 medium = true
                 large = false
@@ -218,24 +278,23 @@ fun TextSize(){
         )
         Text(
             text = "Abc",
-            color = if (large) Color.Black else Color.Gray,
+            color = if (large) color.primary else Color.Gray,
             fontSize = 20.sp,
             fontWeight = if (large) FontWeight.Bold else FontWeight.Normal,
             modifier = Modifier.clickable {
+                onTextSizeSelected(20.sp)
                 small = false
                 medium = false
                 large = true
             }
         )
     }
-
 }
 
 @Preview(showBackground = true)
 @Composable
-fun SettingScreenPreview (){
+fun SettingScreenPreview() {
     SettingsScreen()
 }
-
 
 
