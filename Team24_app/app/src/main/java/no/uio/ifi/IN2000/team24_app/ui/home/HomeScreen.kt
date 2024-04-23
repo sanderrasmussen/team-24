6aa22fc96a1d6767d10803397dcec49926159136
Hifza Mehmood <hifzame@uio.no>
2024-04-24 00:37:18 +0200

    two images on top of each other

--- Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/ui/home/HomeScreen.kt
+++ Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/ui/home/HomeScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -41,6 +42,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material.icons.filled.Close
@@ -149,6 +151,13 @@ fun HomeScreen(navController: NavController,
             contentDescription= null,
             contentScale= ContentScale.FillBounds,
             modifier= Modifier.matchParentSize())
+
+            Image(
+                painter = (painterResource(id = R.drawable.raindrops)),
+                contentDescription = null,
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier.matchParentSize()
+            )
 
     Column(
         //added these two to center the content
