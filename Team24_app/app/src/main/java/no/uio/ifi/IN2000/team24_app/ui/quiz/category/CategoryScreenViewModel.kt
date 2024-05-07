f3a555b788a0934bfdb8974c09da258caafca706
milat <milat@uio.no>
2024-05-07 04:20:31 +0200

    added functionality for fetching category for category screen in category screen view model

--- Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/ui/quiz/category/CategoryScreenViewModel.kt
+++ Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/ui/quiz/category/CategoryScreenViewModel.kt
@@ -1,7 +1,75 @@
 package no.uio.ifi.IN2000.team24_app.ui.quiz.category
 
-class CategoryScreenViewModel {
+import android.content.ContentValues
+import android.content.ContentValues.TAG
+import android.util.Log
+import androidx.annotation.MainThread
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import no.uio.ifi.IN2000.team24_app.data.category.CategoryRepository
+import no.uio.ifi.IN2000.team24_app.data.database.Category
 
+data class CategoryUiState(
 
+    val category: Category? = null
+
+)
+
+class CategoryScreenViewModel: ViewModel() {
+
+    // category repo to fetch category from
+    private val categoryRepository: CategoryRepository = CategoryRepository()
+
+    // ui state values for fetching category
+    private val _categoryUiState = MutableStateFlow(CategoryUiState())
+    val categoryUiState: StateFlow<CategoryUiState> = _categoryUiState.asStateFlow()
+
+    private var initialization = false
+
+    @MainThread
+    fun initialize(categoryName: String) {
+
+        if (!initialization) {
+
+            initialization = true
+            viewModelScope.launch {
+
+                loadCategoryInfo(categoryName)
+
+            }
+
+        }
+
+    }
+
+    // function to fetch categories
+    private fun loadCategoryInfo(categoryName: String) {
+
+        viewModelScope.launch(Dispatchers.IO) {
+
+            try {
+
+                _categoryUiState.update { currentCategoriesUiState ->
+
+                    val category = categoryRepository.getCategory(categoryName)
+                    currentCategoriesUiState.copy(category = category)
+
+                }
+
+            } catch (e: Exception) {
+
+                Log.e(TAG, "Feil ved henting av kategori: ${e.message}", e)
+
+            }
+
+        }
+
+    }
 
 }
