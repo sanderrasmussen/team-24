ad2759e8552e3c6dcd7903b044abae8ec3f38cd7
sanderrasmussen <sander.rasmussn@gmail.com>
2024-05-07 23:54:37 +0200

    refactored storescreenviewmodel, all lag should now be fixed. Also changed storescreen in order to coorporate them together.

--- Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/ui/store/StoreScreenViewModel.kt
+++ Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/ui/store/StoreScreenViewModel.kt
@@ -1,6 +1,5 @@
 package no.uio.ifi.IN2000.team24_app.ui.store
 
-
 import android.content.ContentValues.TAG
 import android.util.Log
 import androidx.compose.runtime.getValue
@@ -32,42 +31,37 @@ import no.uio.ifi.IN2000.team24_app.data.character.legs
 import no.uio.ifi.IN2000.team24_app.data.character.loadSelectedClothes
 import no.uio.ifi.IN2000.team24_app.data.character.torsos
 
+class StoreScreenViewModel(): ViewModel() {
 
-class StoreScreenViewModel(
-    val bankRepository : BankRepository = BankRepository(),
-    val clothesRepository : ClothesRepository= ClothesRepository(),
+    val bankRepository: BankRepository = BankRepository()
+    val clothesRepository: ClothesRepository = ClothesRepository()
 
-    private val _hodePlagg: MutableStateFlow<List<Head>> = MutableStateFlow(emptyList()),
-    val hodePlagg: StateFlow<List<Head>> = _hodePlagg.asStateFlow(),
+    private val _hodePlagg: MutableStateFlow<List<Head>> = MutableStateFlow(loadNotOwnedHeads())
+    val hodePlagg: StateFlow<List<Head>> = _hodePlagg.asStateFlow()
 
-    private val _overdeler: MutableStateFlow<List<Torso>> = MutableStateFlow(emptyList()),
-    val overdeler: StateFlow<List<Torso>> = _overdeler.asStateFlow(),
+    private val _overdeler: MutableStateFlow<List<Torso>> = MutableStateFlow(loadNotOwnedTorsos())
+    val overdeler: StateFlow<List<Torso>> = _overdeler.asStateFlow()
 
-    private val _bukser: MutableStateFlow<List<Legs>> = MutableStateFlow(emptyList()),
-    val bukser: StateFlow<List<Legs>> = _bukser.asStateFlow(),
+    private val _bukser: MutableStateFlow<List<Legs>> = MutableStateFlow(loadNotOwnedLegs())
+    val bukser: StateFlow<List<Legs>> = _bukser.asStateFlow()
 
+    private var _currentSum: MutableStateFlow<Int?> = MutableStateFlow(loadBalanceFromDisk())
+    val currentSum: StateFlow<Int?> = _currentSum.asStateFlow()
 
-
-
-
-): ViewModel() {
-
-    private var _currentSum:MutableStateFlow<Int?> = MutableStateFlow(loadBalanceFromDisk())
-    val currentSum: StateFlow<Int?> =_currentSum.asStateFlow()
-
-    val characterState :MutableStateFlow<Character> = MutableStateFlow(loadClothesFromDisk())
-    var character : StateFlow<Character> = characterState.asStateFlow()
-
+    val characterState: MutableStateFlow<Character> = MutableStateFlow(loadClothesFromDisk())
+    var character: StateFlow<Character> = characterState.asStateFlow()
 
     init {
         //load selected clothes from disk
         viewModelScope.launch {
             characterState.update { loadSelectedClothes() }
             _currentSum.update { bankRepository.getBankBalance() }
+            _hodePlagg.update { clothesRepository.getAllNotOwnedHeads()}
+            _overdeler.update { clothesRepository.getAllNotOwnedTorsos()}
+            _bukser.update { clothesRepository.getAllOwnedLegs()}
         }
-        getPlagg()
-
     }
+
     fun loadClothesFromDisk(): Character {
         var character = getDefaultBackupCharacter()
         runBlocking {
@@ -75,78 +69,67 @@ class StoreScreenViewModel(
         }
         return character
     }
+
     fun loadBalanceFromDisk(): Int? {
-        var balance : Int? = null
+        var balance: Int? = null
         runBlocking {
             balance = bankRepository.getBankBalance()
         }
         return balance
     }
 
-
-    //I assume this is not owned clothes
-    private fun getPlagg() {
-        viewModelScope.launch {
-            try {
-                _hodePlagg.value = hentHodeplagg()
-                _overdeler.value = hentOverdeler()
-                _bukser.value = hentBukser()
-
-            } catch (e: Exception){
-                Log.e(TAG, "An error occurred: ${e.message}", e)
-
-            }
-        }
-    }
-
     fun subtractMoney(clothingPrice: Int) {
         viewModelScope.launch(Dispatchers.IO) {
             try {
                 bankRepository.withdraw(clothingPrice)
-                _currentSum.update { it -> it?.minus(clothingPrice)?:0}
+                _currentSum.update { it -> it?.minus(clothingPrice) ?: 0 }
             } catch (e: Exception) {
                 Log.e(TAG, "Error subtracting money: ${e.message}", e)
             }
         }
     }
-
-    fun unlockPlagg(plagg: Clothing){
+    fun unlockPlagg(plagg: Clothing) {
         viewModelScope.launch(Dispatchers.IO) {
             clothesRepository.setClothingToOwned(plagg.imageAsset)
         }
-        when(plagg){
-
+        when (plagg) {
             is Head -> {
-               val test =  _hodePlagg.value.toMutableList()
+                val test = _hodePlagg.value.toMutableList()
                 test.remove(plagg)
                 _hodePlagg.update { test }
             }
             is Torso -> {
-                val test =  _overdeler.value.toMutableList()
+                val test = _overdeler.value.toMutableList()
                 test.remove(plagg)
                 _overdeler.update { test }
 
             }
-
             is Legs -> {
-                val test =  _bukser.value.toMutableList()
+                val test = _bukser.value.toMutableList()
                 test.remove(plagg)
                 _bukser.update { test }
             }
-
-
         }
     }
-    suspend fun hentHodeplagg(): ArrayList<Head> {
-        return  ArrayList(clothesRepository.getAllNotOwnedHeads())//casting to arraylist, then i dont need to refactor too much
+    fun loadNotOwnedHeads() : List<Head> {
+        var heads: List<Head> = emptyList()
+        runBlocking {
+            heads = clothesRepository.getAllNotOwnedHeads()
+        }
+        return heads
     }
-
-    suspend fun hentOverdeler(): ArrayList<Torso> {
-        return ArrayList(clothesRepository.getAllNotOwnedTorsos())
+    fun loadNotOwnedTorsos() : List<Torso>{
+        var torsos : List<Torso> = emptyList()
+        runBlocking {
+            torsos = clothesRepository.getAllNotOwnedTorsos()
+        }
+        return torsos
     }
-
-    suspend fun hentBukser(): ArrayList<Legs> {
-        return ArrayList(clothesRepository.getAllNotOwnedLegs())
+    fun loadNotOwnedLegs() : List<Legs>{
+        var legs : List<Legs> = emptyList()
+        runBlocking {
+            legs = clothesRepository.getAllNotOwnedLegs()
+        }
+        return legs
     }
 }
-
