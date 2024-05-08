8fa1bec0ca34d15f3648a40d44c11553edc7f2ef
milat <milat@uio.no>
2024-05-08 05:49:20 +0200

    added farevarsel questions to database

--- Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/data/database/application.kt
+++ Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/data/database/application.kt
@@ -47,14 +47,91 @@ class AppDatabaseCallback : RoomDatabase.Callback() {
 
             // categories:
             val omVaeret = Category("Om været")
-            val farevarsler = Category("Farevarsler")
-            val oeving = Category("Farevarsler", 10, false)
             MyDatabase.getInstance().categoryDao().insertAll(omVaeret)
+
+            val farevarsler = Category("Farevarsler")
             MyDatabase.getInstance().categoryDao().insertAll(farevarsler)
+
+            val oeving = Category("Farevarsler", 5, false)
             MyDatabase.getInstance().categoryDao().insertAll(oeving)
 
+            // questions:
+
+            // farevarsel questions:
+            val roedtFarevarselSpoersmaal1 = Question(
+
+                "Hvilken av disse gir indikasjon på rødt farevarsel?",
+                "Farevarsler",
+                listOf("Lett snø", "Orkan", "2 mm regn", "Grå skyer"),
+                1
+
+            )
+            MyDatabase.getInstance().questionDao().insertAll(roedtFarevarselSpoersmaal1)
+
+            val roedtFarevarselSpoersmaal2 = Question(
+
+                "Hvilken av disse gir indikasjon på rødt farevarsel?",
+                "Farevarsler",
+                listOf("Tordenstorm", "Lett tåke", "Alvorlig jordskjelv", "Sterk vind"),
+                2
+
+            )
+            MyDatabase.getInstance().questionDao().insertAll(roedtFarevarselSpoersmaal2)
+
+            val roedtFarevarselSpoersmaal3 = Question(
+
+                "Hvilken av disse gir indikasjon på rødt farevarsel?",
+                "Farevarsler",
+                listOf("Kraftig tåke", "Eksplosjoner", "Kraftig regn", "Alvorlig luftforurensning"),
+                3
+
+            )
+            MyDatabase.getInstance().questionDao().insertAll(roedtFarevarselSpoersmaal3)
+
+            val gultFarevarselSpoersmaal1 = Question(
+
+                "Hvilken av disse gir indikasjon på gult farevarsel?",
+                "Farevarsler",
+                listOf("Kraftig vind", "Orkan", "Moderat mengde snø", "Solskinn"),
+                2
+
+            )
+            MyDatabase.getInstance().questionDao().insertAll(gultFarevarselSpoersmaal1)
+
+            val gultFarevarselSpoersmaal2 = Question(
+
+                "Hvilken av disse gir indikasjon på gult farevarsel?",
+                "Farevarsler",
+                listOf("Lett bris", "Tsunami", "Kraftig tordenvær", "Snøstorm"),
+                0
+
+            )
+            MyDatabase.getInstance().questionDao().insertAll(gultFarevarselSpoersmaal2)
+
+            val gultFarevarselSpoersmaal3 = Question(
+
+                "Hvilken av disse gir indikasjon på gult farevarsel?",
+                "Farevarsler",
+                listOf("Ekstrem hete", "Moderat regn", "Alvorlig luftforurensning", "Kraftig jordskjelv"),
+                2
+
+            )
+            MyDatabase.getInstance().questionDao().insertAll(gultFarevarselSpoersmaal3)
+
+            val vindkastFarevarselAarsakerSpoersmaal = Question(
+
+                "Hvilken av disse er en av de vanligste årsakene til at meterologiske tjenester utsender farevarsel for kraftige vindkast?",
+                "Farevarsler",
+                listOf("Sterke stormsystemer", "Vind fra fjell- eller delstrøk", "Økt solaktivitet", "Tørkeperioder"),
+                0
+
+            )
+            MyDatabase.getInstance().questionDao().insertAll(vindkastFarevarselAarsakerSpoersmaal)
+
         }
+
     }
+
 }
 
 
