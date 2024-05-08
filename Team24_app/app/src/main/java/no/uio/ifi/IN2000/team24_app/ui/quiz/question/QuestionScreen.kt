8912da4ce9f48934e1d7b4263a5d87dfdfbaf723
milat <milat@uio.no>
2024-05-08 08:34:47 +0200

    removed unused imports in question screen

--- Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/ui/quiz/question/QuestionScreen.kt
+++ Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/ui/quiz/question/QuestionScreen.kt
@@ -16,8 +16,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
