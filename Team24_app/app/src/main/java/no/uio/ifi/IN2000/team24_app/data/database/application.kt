a1dcee3f1b99c4e704b7b7f225b8034339186ff2
milat <milat@ulrik.uio.no>
2024-05-09 15:53:32 +0200

    added some alt images and inserted new clothes into database

--- Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/data/database/application.kt
+++ Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/data/database/application.kt
@@ -49,26 +49,46 @@ class AppDatabaseCallback : RoomDatabase.Callback() {
             //clothing tables:
 
             //heads:
-            val short_hair :Clothes= Clothes(R.drawable.head_short_hair,"Short Hair", 25,  30, R.drawable.alt_head_short_hair,"head",true)
-            val paint_head : Clothes = Clothes(R.drawable.paint_head, "Paint", 0, 1000, R.drawable.paint_head_alt,"head",false)
+            val short_hair = Clothes(R.drawable.head_short_hair,"Short Hair", 25,  30, R.drawable.alt_head_short_hair,"head",true)
+            val paint_head = Clothes(R.drawable.paint_head, "Paint", 0, 1000, R.drawable.paint_head_alt,"head",false)
+            val hat = Clothes(R.drawable.alt_head_short_hair_hat, "Hat", 0, 30, R.drawable.alt_head_short_hair_hat, "head", false)
             MyDatabase.getInstance().clothesDao().insertAll(short_hair)
             MyDatabase.getInstance().clothesDao().insertAll(paint_head)
+            MyDatabase.getInstance().clothesDao().insertAll(hat)
 
             //torsos:
             val paintTorso = Clothes(R.drawable.paint_torso,"Paint", 0,  1000, R.drawable.paint_torso_alt,"torso", false )
             val long_sleeve = Clothes(R.drawable.torso_long_sleeves, "Long Sleeve", 5,25, R.drawable.alt_torso_long_sleeve, "torso", true)
             val short_sleeve = Clothes(R.drawable.torso_short_sleeves,"Short Sleeve", 25,  15, R.drawable.alt_torso_short_sleeve, "torso", false)
+            val short_sleeve_shirt = Clothes(R.drawable.torso_short_sleeves_shirt,"Short Sleeve Shirt", 25,  15, R.drawable.alt_torso_short_sleeves_shirt, "torso", false)
+            val short_sleeve_shirt_leaves = Clothes(R.drawable.torso_short_sleeves_leaves,"Short Sleeve Shirt Leaves", 25,  15, R.drawable.alt_torso_short_sleeves_leaves, "torso", false)
+            val long_sleeve_hoodie = Clothes(R.drawable.torso_long_sleeves_hoodie,"Long Sleeve Hoodie", 5, 15, R.drawable.alt_torso_long_sleeves_hoodie, "torso", false)
+            val long_sleeve_jacket = Clothes(R.drawable.torso_long_sleeves_jacket,"Long Sleeve Jacket", 0, 25, R.drawable.alt_torso_long_sleeves_jacket, "torso", false)
+            val long_sleeve_raincoat = Clothes(R.drawable.torso_long_sleeves_raincoat,"Long Sleeve Raincoat", 5, 25, R.drawable.alt_torso_long_sleeves_raincoat, "torso", false)
+            val long_sleeve_warm_coat = Clothes(R.drawable.torso_long_sleeves_warm_coat,"Long Sleeve Warm Coat", -10, 25, R.drawable.alt_torso_long_sleeves_warm_coat, "torso", false)
+            val long_sleeve_warm_jacket = Clothes(R.drawable.torso_long_sleeves_warm_coat,"Long Sleeve Warm Jacket", -10, 25, R.drawable.alt_torso_long_sleeves_warm_jacket, "torso", false)
             MyDatabase.getInstance().clothesDao().insertAll(paintTorso)
             MyDatabase.getInstance().clothesDao().insertAll(long_sleeve)
             MyDatabase.getInstance().clothesDao().insertAll(short_sleeve)
+            MyDatabase.getInstance().clothesDao().insertAll(short_sleeve_shirt)
+            MyDatabase.getInstance().clothesDao().insertAll(short_sleeve_shirt_leaves)
+            MyDatabase.getInstance().clothesDao().insertAll(long_sleeve_hoodie)
+            MyDatabase.getInstance().clothesDao().insertAll(long_sleeve_jacket)
+            MyDatabase.getInstance().clothesDao().insertAll(long_sleeve_raincoat)
+            MyDatabase.getInstance().clothesDao().insertAll(long_sleeve_warm_coat)
+            MyDatabase.getInstance().clothesDao().insertAll(long_sleeve_warm_jacket)
 
             //legs:
             val paintLegs = Clothes(R.drawable.paint_legs,"Paint", 0,  1000, R.drawable.paint_legs_alt, "legs", false)
             val pants = Clothes(R.drawable.legs_pants,"Pants", 5,  25, R.drawable.alt_legs_pants, "legs", true)
             val shorts = Clothes(R.drawable.legs_shorts,"Shorts", 25,  15, R.drawable.alt_legs_shorts, "legs", false)
+            val shorts_pink = Clothes(R.drawable.legs_shorts_pink,"Pink Shorts", 25,  15, R.drawable.alt_legs_shorts_pink, "legs", false)
+            val pants_black = Clothes(R.drawable.legs_pants_black,"Black Pants", 5,  15, R.drawable.alt_legs_pants_black, "legs", false)
             MyDatabase.getInstance().clothesDao().insertAll(paintLegs)
             MyDatabase.getInstance().clothesDao().insertAll(pants)
             MyDatabase.getInstance().clothesDao().insertAll(shorts)
+            MyDatabase.getInstance().clothesDao().insertAll(pants_black)
+            MyDatabase.getInstance().clothesDao().insertAll(shorts_pink)
 
             //setting inital selected clothing
             //inserting equiped clothes table
