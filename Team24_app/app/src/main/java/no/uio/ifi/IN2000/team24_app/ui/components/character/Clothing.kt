8a02c733c1af908faf7531a6881257a678adb358
halvornj <halvornjaatad@gmail.com>
2024-05-09 18:40:27 +0200

    I've fixed the problem with the layout of the inventory menu(hopefully), but I can't actually test it without at least 3 of one type of clothing
    and there are no more articles of clothing available.

--- Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/ui/components/character/Clothing.kt
+++ Team24_app/app/src/main/java/no/uio/ifi/IN2000/team24_app/ui/components/character/Clothing.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -85,8 +86,8 @@ fun Inventory(characterState: MutableStateFlow<Character>, modifier: Modifier = 
         if (showInventory) {
             ClothingMenuCard(
                 character = character,
-                closeFunction = ::selectedClothing,
-                modifier = Modifier    //thats right, we doing function invocation in the parameter list
+                closeFunction = ::selectedClothing, //thats right, we doing function invocation in the parameter list
+                modifier = Modifier
                     .fillMaxWidth(0.8f)
                     .fillMaxHeight(0.6f)
                     .padding(5.dp)
@@ -127,28 +128,31 @@ fun ClothingMenu(closeFunction: (clothing: Clothing) -> Unit, modifier: Modifier
             .padding(10.dp)
 
     ) {
-        Text(text = "Heads:")
         LazyVerticalGrid(columns = GridCells.Fixed(2),) {
+            item { Text(text = "Hoder:") }
+            item{ Spacer(modifier = Modifier) }
+
             items(heads()) { head ->
                 ClothingCard(clothing = head, closeFunction = closeFunction)
-
             }
-        }
-        Text(text = "Tops:")
-        LazyVerticalGrid(columns = GridCells.Fixed(2),) {
+            if(heads().size % 2 == 1) item { Spacer(modifier = Modifier) }
+
+            item { Text(text = "Overdel:") }
+            item{ Spacer(modifier = Modifier) }
             items(torsos()) { torso ->
                 ClothingCard(clothing = torso, closeFunction = closeFunction)
             }
-        }
-        Text(text = "Bottoms:")
+            if(torsos().size % 2 == 1) item { Spacer(modifier = Modifier) }
 
-        LazyVerticalGrid(columns = GridCells.Fixed(2),) {
+            item { Text(text = "Underdel:") }
+            item{ Spacer(modifier = Modifier) }
             items(legs()) { leg ->
                 ClothingCard(clothing = leg, closeFunction = closeFunction)
             }
+            if(legs().size % 2 == 1) item { Spacer(modifier = Modifier) }
         }
     }
-    }
+}
 
 @Composable
 fun ClothingCard(
