1ce47d69052aafe0301ddefdae0f7bea10f3bb72
sanderrasmussen <sander.rasmussn@gmail.com>
2024-05-14 16:52:35 +0200

    Made unit tests for locationforecastdatasource and forecast

--- Team24_app/app/src/test/java/no/uio/ifi/IN2000/team24_app/data/locationForecast/locationForecastDatasourceKtTest.kt
+++ Team24_app/app/src/test/java/no/uio/ifi/IN2000/team24_app/data/locationForecast/locationForecastDatasourceKtTest.kt
@@ -1,16 +1,23 @@
 package no.uio.ifi.IN2000.team24_app.data.locationForecast
 
+import junit.framework.TestCase.assertNotNull
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.SerialName
 import org.junit.Test
 
 class locationForecastDatasourceKtTest {
 
     val source = LocationForecastDatasource()
-     @Test
-     fun testForecast(){
-         runBlocking {
-             val forecast = source.getLocationForecastData(59.0, 10.0)
+    @Test
+    fun test(){
+        runBlocking {
+            val forecast = source.getLocationForecastData(59.0, 10.0)
             println(forecast)
-         }
-     }
+            assertNotNull(forecast)
+            assertNotNull(forecast?.type)
+            assertNotNull(forecast?.geometry)
+            assertNotNull(forecast?.properties)
+
+        }
+    }
 }
--- Team24_app/app/src/test/java/no/uio/ifi/IN2000/team24_app/data/locationForecast/locationForecastRepositoryTest.kt
+++ Team24_app/app/src/test/java/no/uio/ifi/IN2000/team24_app/data/locationForecast/locationForecastRepositoryTest.kt
@@ -1,66 +1,29 @@
 package no.uio.ifi.IN2000.team24_app.data.locationForecast
 
-import android.util.Log
-import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Assert.assertNotNull
 import org.junit.Test
-data class weatherForecastUIState(
-    val weatherNow : WeatherDetails?,
-    val forecastMap : HashMap<String?, ArrayList<WeatherDetails>>?
-)
-/*class locationForecastRepositoryTest {
-    val repo : LocationForecastRepository = LocationForecastRepository()
+
+//REMEMBER TO REMOVE LOG.d FROM FUNCTIONS IN ORDER TO MAKE THE TESTS WORK
+class LocationForecastRepositoryTest {
+
+    // You can mock dependencies and test the behavior of your repository methods
+    private val forecastRepo = LocationForecastRepository()
+    val locationForecast: LocationForecast? =
+        runBlocking { forecastRepo.fetchLocationForecast(59.91, 10.75) }
+
     @Test
-    fun testCurrentWeather(){
-        runBlocking{
-            repo.fetchLocationForecast(59.0, 10.0)
+    fun tests() {
+        //assertNotNull(locationForecast)
 
+        val timeseries = forecastRepo.getTimeseries()
+        // Ensure that the timeseries is not null
+        //assertNotNull(timeseries)
 
-            println("-------------- Test start ---------------")
-
-            """ TESTER STATEFLOW, NÅR DETTE TESTES VIL PRINTING I TERMINALEN ALDRIG TERMINERE, MEN SER UT TIL AT DET FUNGERER ALLIKEVEL
-            repo.ObserveCurrentWeather().collect { weatherNow ->
-                println(weatherNow?.time)
-                println(weatherNow?.air_pressure_at_sea_level)
-                println(weatherNow?.air_temperature)
-                println(weatherNow?.cloud_area_fraction)
-                println(weatherNow?.relative_humidity)
-                println(weatherNow?.wind_from_direction)
-                println(weatherNow?.wind_speed)
-                println(weatherNow?.next_1_hours_symbol_code)
-                println(weatherNow?.next_1_hours_precipitation_amount)
-            }"""
-
-            repo.ObserveNext6DaysForecast().collect { day ->
-                println(day?.get(0)?.time)
-                println(day?.get(0)?.air_pressure_at_sea_level)
-                println(day?.get(0)?.air_temperature)
-                println(day?.get(0)?.cloud_area_fraction)
-                println(day?.get(0)?.relative_humidity)
-                println(day?.get(0)?.wind_from_direction)
-                println(day?.get(0)?.wind_speed)
-                println(day?.get(0)?.next_1_hours_symbol_code)
-                println(day?.get(0)?.next_1_hours_precipitation_amount)
-            }
-
-            """
-            println(weatherNow?.time)
-            println(weatherNow?.air_pressure_at_sea_level)
-            println(weatherNow?.air_temperature)
-            println(weatherNow?.cloud_area_fraction)
-            println(weatherNow?.relative_humidity)
-            println(weatherNow?.wind_from_direction)
-            println(weatherNow?.wind_speed)
-            println(weatherNow?.next_1_hours_symbol_code)
-            println(weatherNow?.next_1_hours_precipitation_amount)
-            println("----mapTest----")
-
-            //oops datoen i testfilen må endres frem i tid slik at den ikke blir null
-            println(map?.get("2024-03-18")?.size)
-            println(map?.get("2024-03-18")?.get(0)?.time)"""
-            println("-------------- Test slutt ---------------")
-        }
+        val weatherDetail = forecastRepo.createWeatherDetailObject(0)
+        // Ensure that the created weather detail object is not null
+        assertNotNull(weatherDetail)
+// Add more assertions based on the expected behavior of the method
     }
 }
-
- */
