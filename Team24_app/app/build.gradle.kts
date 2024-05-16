dfad9229f29b8d97e5b8d999fc25e619f9ce2f49
halvornj <halvornjaatad@gmail.com>
2024-05-16 15:01:26 +0200

    changed app name

--- Team24_app/app/build.gradle.kts
+++ Team24_app/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         applicationId = "no.uio.ifi.IN2000.team24_app"
-        minSdk = 26 //Build.VERSION_CODES.O everywhere
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
